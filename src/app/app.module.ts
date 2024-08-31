bbaef809db14ec2495c305c3ea20686c9bdfde7b
Jasmin Džubur <dzuburjasmin13@gmail.com>
2024-08-31 19:48:25 +0200

    initial commit
    first 2 sections done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FirstSectionComponent } from './first-section/first-section.component';
+import { SecondSectionComponent } from './second-section/second-section.component';
+import { ThirdSectionComponent } from './third-section/third-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FirstSectionComponent,
+    SecondSectionComponent,
+    ThirdSectionComponent
   ],
   imports: [
     BrowserModule
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
